127ccdf608c580e93b410e55ccdf51203ed84515
Matias Ortiz <ortiz.matias@yahoo.com.ar>
2021-06-19 00:49:04 -0300

    using yfinance

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # investments
+
+python main.py
+```python
+* AGRO.BA > Precio de cierre (Pt-1): 26.9 | Pt-225.9 | Pt-1 - Pt-21.0
+* ADGO.BA > Precio de cierre (Pt-1): 3595.0 | Pt-23625.0 | Pt-1 - Pt-2-30.0
+* AMZN.BA > Precio de cierre (Pt-1): 4047.0 | Pt-24050.0 | Pt-1 - Pt-2-3.0
+* BABA.BA > Precio de cierre (Pt-1): 3937.5 | Pt-23939.5 | Pt-1 - Pt-2-2.0
+* GLNT.BA > Precio de cierre (Pt-1): 6156.5
+* MELI.BA > Precio de cierre (Pt-1): 4088.5 | Pt-24077.5 | Pt-1 - Pt-211.0
+* TSLA.BA > Precio de cierre (Pt-1): 6888.5 | Pt-26909.0 | Pt-1 - Pt-2-20.5
+* GOLD.BA > Precio de cierre (Pt-1): 3467.5
+* LMT.BA > Precio de cierre (Pt-1): 319550.0 | Pt-23190.0 | Pt-1 - Pt-2316360.0
+* MSFT.BA > Precio de cierre (Pt-1): 4361.5 | Pt-24340.5 | Pt-1 - Pt-221.0
+```
---
+++ main.py
@@ -0,0 +1,56 @@
+# usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = "Matias Ortiz"
+__email__ = "matias.ortiz@bvstv.com"
+__webpage__ = "https://github.com/mortiz-code"
+__version__ = "0.1.0"
+__copyright__ = "Copyright (c) 2021, all rights reserved."
+__license__ = "BSD 3-Clause License."
+
+"""
+ Doc: https://colab.research.google.com/drive/1qPNREasgE0vhVKttNL0YyMdZE84n28_H?usp=sharing
+ import datetime
+ now = datetime.datetime.now().strftime("%Y-%m-%d")
+ import investpy
+ investpy.get_stock_historical_data(stock='AGRO',country='argentina',from_date=end, to_date=now,interval='Daily')
+"""
+
+import yfinance as yf
+
+# import pandas as pd
+
+# excel_file = "/mnt/c/Users/mortiz/Dropbox/Inversion.xlsx"
+
+# df = pd.read_excel(excel_file, sheet_name="Cedears")
+
+# data_from_excel = df.iloc[1:11, 0]
+
+# stocks_list = data_from_excel.tolist()
+# import datetime
+# now = datetime.datetime.now().strftime("%Y-%m-%d")
+stocks_list = [
+    "AGRO.BA",
+    "ADGO.BA",
+    "AMZN.BA",
+    "BABA.BA",
+    "GLNT.BA",
+    "MELI.BA",
+    "TSLA.BA",
+    "GOLD.BA",
+    "LMT.BA",
+    "MSFT.BA",
+]
+
+for stock in stocks_list:
+    symbol = yf.Ticker(stock)
+    data = symbol.history(period="2d")
+    close_price = round(data["Close"][0], 2)
+    try:
+        yesterday_close_price = round(data["Close"][1], 2)
+        diff = close_price - yesterday_close_price
+        print(
+            f"* {symbol.ticker} > Precio de cierre (Pt-1): {close_price} | Pt-2{yesterday_close_price} | Pt-1 - Pt-2{diff}"
+        )
+    except:
+        print(f"* {symbol.ticker} > Precio de cierre (Pt-1): {close_price}")
