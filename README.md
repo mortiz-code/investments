e0a0e2a9e489f2cff531013efb4a9450c25b0b5d
Matias Ortiz <ortiz.matias@yahoo.com.ar>
2021-06-22 00:53:00 -0300

    new features

--- README.md
+++ README.md
@@ -1,15 +1,32 @@
 # investments
 
-python main.py
 ```python
-* AGRO.BA > Precio de cierre (Pt-1): 26.9 | Pt-2: 25.9 | Diff: 1.0
-* ADGO.BA > Precio de cierre (Pt-1): 3595.0 | Pt-2: 3625.0 | Diff: -30.0
-* AMZN.BA > Precio de cierre (Pt-1): 4047.0 | Pt-2: 4050.0 | Diff: -3.0
-* BABA.BA > Precio de cierre (Pt-1): 3937.5 | Pt-2: 3939.5 | Diff: -2.0
-* GLNT.BA > Precio de cierre (Pt-1): 6156.5
-* MELI.BA > Precio de cierre (Pt-1): 4088.5 | Pt-2: 4077.5 | Diff: 11.0
-* TSLA.BA > Precio de cierre (Pt-1): 6888.5 | Pt-2: 6909.0 | Diff: -20.5
-* GOLD.BA > Precio de cierre (Pt-1): 3467.5
-* LMT.BA > Precio de cierre (Pt-1): 319550.0 | Pt-2: 3190.0 | Diff: 316360.0
-* MSFT.BA > Precio de cierre (Pt-1): 4361.5 | Pt-2: 4340.5 | Diff: 21.0
+(venv) ┌──(mat㉿kali)-[~/github/devnet/stocks] (main)
+└─$ /home/mat/github/devnet/stocks/venv/bin/python /home/mat/github/devnet/stocks/main.py
+
+--------------------- Information extraida de YahooFinance ---------------------
+* AGRO.BA > Pt-1: 26.9 | Pt-1 - Pt2: 1.0
+* ADGO.BA > Pt-1: 3595.0 | Pt-1 - Pt2: -30.0
+* AMZN.BA > Pt-1: 4047.0 | Pt-1 - Pt2: -3.0
+* BABA.BA > Pt-1: 3937.5 | Pt-1 - Pt2: -2.0
+* GLNT.BA > Precio de cierre: 6156.5
+* MELI.BA > Pt-1: 4088.5 | Pt-1 - Pt2: 11.0
+* TSLA.BA > Pt-1: 6888.5 | Pt-1 - Pt2: -20.5
+* GOLD.BA > Precio de cierre: 3467.5
+* LMT.BA > Pt-1: 3195.5 | Pt-1 - Pt2: 5.5
+* MSFT.BA > Pt-1: 4361.5 | Pt-1 - Pt2: 21.0
+
+----------------- Diferencia con respecto al precio de compra ------------------
+* AGRO.BA > 6.4 == 0.31%
+* ADGO.BA > 745.0 == 0.26%
+* AMZN.BA > 688.0 == 0.2%
+* BABA.BA > -1076.0 == -0.21%
+* GLNT.BA > 506.5 == 0.09%
+* MELI.BA > 241.5 == 0.06%
+* TSLA.BA > -1401.5 == -0.17%
+* GOLD.BA > 265.5 == 0.08%
+* LMT.BA > -164.5 == -0.05%
+* MSFT.BA > 810.5 == 0.23%
+
+-------- El capital en renta viable a la fecha 2021-06-22 es de 76732.5 --------                             
 ```
--- main.py
+++ main.py
@@ -17,40 +17,85 @@ __license__ = "BSD 3-Clause License."
 """
 
 import yfinance as yf
+import pandas as pd
+import datetime
 
-# import pandas as pd
 
-# excel_file = "/mnt/c/Users/mortiz/Dropbox/Inversion.xlsx"
+def read_excel():
+    excel_file = "/mnt/c/Users/mortiz/Dropbox/Inversion.xlsx"
+    df = pd.read_excel(excel_file, sheet_name="Cedears")
+    data_from_excel_stock = df.iloc[1:11, 0]
+    data_from_excel_q = df.iloc[1:11, 1]
+    data_from_excel_pc = df.iloc[1:11, 3]
+    stocks_list = data_from_excel_stock.tolist()
+    q_list = data_from_excel_q.tolist()
+    pc_list = data_from_excel_pc.tolist()
 
-# df = pd.read_excel(excel_file, sheet_name="Cedears")
+    # Listado de cedears tener el cuenta los nombres porque en BBVA no aparecen los standards de Yahoo Finance.
+    # result = [i + ".ba" for i in stocks_list]
+    # Por el momento uso lista manual porque falla excel.
+    stocks_list = [
+        "AGRO.BA",
+        "ADGO.BA",
+        "AMZN.BA",
+        "BABA.BA",
+        "GLNT.BA",
+        "MELI.BA",
+        "TSLA.BA",
+        "GOLD.BA",
+        "LMT.BA",
+        "MSFT.BA",
+    ]
 
-# data_from_excel = df.iloc[1:11, 0]
+    return stocks_list, q_list, pc_list
 
-# stocks_list = data_from_excel.tolist()
-# import datetime
-# now = datetime.datetime.now().strftime("%Y-%m-%d")
-stocks_list = [
-    "AGRO.BA",
-    "ADGO.BA",
-    "AMZN.BA",
-    "BABA.BA",
-    "GLNT.BA",
-    "MELI.BA",
-    "TSLA.BA",
-    "GOLD.BA",
-    "LMT.BA",
-    "MSFT.BA",
-]
 
-for stock in stocks_list:
-    symbol = yf.Ticker(stock)
-    data = symbol.history(period="2d")
-    close_price = round(data["Close"][0], 2)
+def get_info(stocks_list):
+    print("")
+    print(f" Information extraida de YahooFinance ".center(80, "-"))
+    prices = []
+    for stock in stocks_list:
+        symbol = yf.Ticker(stock)
+        try:
+            data = symbol.history(period="2d")
+            close_price = round(data["Close"][0], 2)
+            yesterday_close_price = round(data["Close"][1], 2)
+            diff = close_price - round(yesterday_close_price, 2)
+            print(f"* {symbol.ticker} > Pt-1: {close_price} | Pt-1 - Pt2: {diff}")
+            prices.append(close_price)
+        except IndexError:
+            print(f"* {symbol.ticker} > Precio de cierre: {close_price}")
+            prices.append(close_price)
+
+    return prices
+
+
+def beneficio(prices, q_list, pc_list, stocks_list):
+    now = datetime.datetime.now().strftime("%Y-%m-%d")
+    total = 0
+    k = [x * y for x, y in zip(prices, q_list)]
+    for i in k:
+        total += i
+
+    print("")
+
+    print(" Diferencia con respecto al precio de compra ".center(80, "-"))
+    a = zip(stocks_list, prices, pc_list)
+    for i in a:
+        print(f"* {i[0]} > {round(i[1] - i[2],2)} == {round((i[1]-i[2])/i[2],2)}%")
+
+    print("")
+    print(f" El capital en renta viable a la fecha {now} es de {total} ".center(80, "-"))
+
+
+def main():
+    stocks_list, q_list, pc_list = read_excel()
+    prices = get_info(stocks_list)
+    beneficio(prices, q_list, pc_list, stocks_list)
+
+
+if __name__ == "__main__":
     try:
-        yesterday_close_price = round(data["Close"][1], 2)
-        diff = close_price - yesterday_close_price
-        print(
-            f"* {symbol.ticker} > Precio de cierre (Pt-1): {close_price} | Pt-2: {yesterday_close_price} | Diff: {diff}"
-        )
-    except:
-        print(f"* {symbol.ticker} > Precio de cierre (Pt-1): {close_price}")
+        main()
+    except KeyboardInterrupt as e:
+        exit()
