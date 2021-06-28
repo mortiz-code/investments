12891995933a625219ce09731db96e5f8bd6c536
Matias Ortiz <ortiz.matias@yahoo.com.ar>
2021-06-28 01:21:49 -0300

    Revert "new version"
    This reverts commit 7ac5959362f093a20a2df1524e91b87cc6d36767.

--- main.py
+++ main.py
@@ -5,7 +5,7 @@
 __author__ = "Matias Ortiz"
 __email__ = "matias.ortiz@bvstv.com"
 __webpage__ = "https://github.com/mortiz-code"
-__version__ = "1.1.0"
+__version__ = "0.1.0"
 __copyright__ = "Copyright (c) 2021, all rights reserved."
 __license__ = "BSD 3-Clause License."
 
