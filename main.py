38f8651c3e471ae4e3c5323124b2fd5007191094
mortiz-code <matcode@pm.me>
2023-04-25 15:12:46 -0300

    Add: BYMA ON

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 # usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-
 __author__ = "Matias Ortiz"
 __email__ = "matias.ortiz@bvstv.com"
 __webpage__ = "https://github.com/mortiz-code"
@@ -9,69 +8,84 @@ __version__ = "1.1.0"
 __copyright__ = "Copyright (c) 2021, all rights reserved."
 __license__ = "BSD 3-Clause License."
 
+import requests as req
+import pandas as pd
+import streamlit as st
+import urllib3
+from ratelimit import limits, sleep_and_retry
 
-import yfinance as yf
-from csv import reader
-from tabulate import tabulate
-from sys import argv
-from os.path import isfile
+urllib3.disable_warnings()
+
+# URL = "https://open.bymadata.com.ar/vanoms-be-core/rest/api/bymadata/free/market-time"
+HEADERS = {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+    "Referer-Policy": "no-referrer-when-downgrade",
+}
 
 
-def info(stocks_list):
-    d, e, f, h = [], [], [], []
-    for stock in stocks_list:
-        data = yf.download(
-            tickers=stock,
-            period="1d",
-            interval="1m",
-            progress=False,
-            auto_adjust=False,
-            prepost=True,
-            rounding=True,
-        )
-        actual_price = data.tail(1)["Close"][0]
-        d.append(f"{stock}".upper())
-        e.append(f"$ {actual_price}")
-        symbol = yf.Ticker(stock)
-        data = symbol.history(period="2d")
-        yerterday_close_price = data["Close"][0]
-        diff = round(actual_price - yerterday_close_price, 2)
-        f.append(f"$ {diff}")
-    for i in f:
-        if "-" in i:
-            h.append(f"[-]")
-        else:
-            h.append(f"[+]")
-    g = zip(d, e, f, h)
-    print(
-        tabulate(
-            list(g),
-            headers=["Stock", "Current", "Diff", "Variation"],
-            tablefmt="simple",
-        )
+def test_details(symbol):
+    st.code(f" {symbol} ".center(80, "-"))
+
+
+@sleep_and_retry
+@limits(calls=10, period=30)
+def details(symbol):
+    DATA = {"symbol": symbol, "Content-Type": "application/json"}
+    URL = "https://open.bymadata.com.ar/vanoms-be-core/rest/api/bymadata/free/bnown/fichatecnica/especies/general"
+    r = req.post(URL, timeout=10, json=DATA, headers=HEADERS, stream=False, verify=False)
+    # st.json(r.json()["data"][0])  # Json crudo
+    st.code(f" {symbol} ".center(80, "-"))
+    if r.status_code == 200:
+        df = pd.DataFrame(r.json()["data"])
+        df = df.loc[
+            :,
+            [
+                "emisor",
+                "moneda",
+                "interes",
+                "denominacionMinima",
+                "formaAmortizacion",
+                "paisLey",
+                "default",
+                "fechaDevenganIntereses",
+            ],
+        ]
+        df = df.iloc[0]
+        st.dataframe(df, use_container_width=True)
+    else:
+        st.error(f"Error: {r.status_code}")
+
+
+def on():
+    DATA = {
+        "excludeZeroPFAndQty": True,
+        "T2": True,
+        "T1": False,
+        "T0": False,
+        "Content-Type": "application/json",
+    }
+    URL = (
+        "https://open.bymadata.com.ar/vanoms-be-core/rest/api/bymadata/free/negociable-obligations"
     )
-
-
-def read(file):
-    try:
-        with open(file, "r") as f:
-            data = reader(f)
-            for i in data:
-                info(i)
-    except FileNotFoundError:
-        print(f"File {file} not found.")
-        exit()
+    r = req.post(URL, timeout=10, json=DATA, headers=HEADERS, stream=False, verify=False)
+    if r.status_code == 200:
+        df = pd.DataFrame(r.json())
+        df = df.loc[
+            :, ["symbol", "denominationCcy", "daysToMaturity", "settlementPrice", "imbalance"]
+        ]
+        # for i in range(5): # Test las primeros 5 ON
+        #     details(df.symbol.iloc[i])
+        st.dataframe(df, use_container_width=True)
+        with st.spinner("In progress..."):
+            for i in df.symbol:
+                details(i)
+    else:
+        st.error(f"Error: {r.status_code}")
 
 
 def main():
-    if len(argv) == 2:
-        file = argv[1]
-        read(file)
-    elif isfile("portfolio.csv"):
-        read("portfolio.csv")
-    else:
-        print("CSV file not found.")
-        exit()
+    on()
 
 
 if __name__ == "__main__":
